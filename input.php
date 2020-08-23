414c1cd0ecd45c5e5aef834fe3bd7aa103a0b521
4happy-studio <51131436+4happy-studio@users.noreply.github.com>
2020-08-23 22:16:34 +0700

    Input produk

--- input.php
+++ input.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Add Tenant Form</title>
+<title>Add Input Form</title>
 </head>
 <body>
+
+<h2>INPUT TENANT</h2>
 <form action="input_tenant.php" method="post">
     <p>
         <label for="id">ID:</label>
@@ -22,6 +24,36 @@
         <label for="cluster">cluster</label>
         <input type="text" name="cluster" id="cluster">
     </p>
+    <input type="submit" value="Submit">
+</form>
+
+<br>
+<br>
+
+<h2>INPUT PRODUK</h2>
+<form action="input_produk.php" method="post">
+    <p>
+        <label for="id">ID:</label>
+        <input type="text" name="id" id="id">
+    </p>
+    <p>
+        <label for="id_tenant">ID Tenant:</label>
+        <input type="text" name="id_tenant" id="id_tenant">
+    </p>
+    <p>
+        <label for="name">Nama produk:</label>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="desc">Deskripsi produk</label>
+        <input type="text" name="desc" id="desc">
+    </p>
+
+    <p>
+        <label for="img">Gambar Produk</label>
+        <input type="file" id="img" name="img" accept="image/*">
+    </p>
+    
     <input type="submit" value="Submit">
 </form>
 </body>
--- input_produk.php
+++ input_produk.php
@@ -1,4 +1,25 @@
 <?php
+
+// ambil data file
+$namaFile = $_FILES['img']['name'];
+$namaSementara = $_FILES['img']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "assets/img/produk";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+?>
+
+<?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("bikepriexpo.com:3306", "bikepri_user", "exploit9827", "bikepri_produk");
@@ -10,12 +31,12 @@ if($link === false){
  
 // Escape user inputs for security
 $id = mysqli_real_escape_string($link, $_REQUEST['id']);
+$id_tenant = mysqli_real_escape_string($link, $_REQUEST['id_tenant']);
 $name = mysqli_real_escape_string($link, $_REQUEST['name']);
 $desc = mysqli_real_escape_string($link, $_REQUEST['desc']);
-$cluster = mysqli_real_escape_string($link, $_REQUEST['cluster']);
 
 // Attempt insert query execution
-$sql = "INSERT INTO tenant (`id`, `name`, `desc`, `cluster`) VALUES ('$id', '$name', '$desc', '$cluster')";
+$sql = "INSERT INTO produk (`id`, `id_tenant`, `name`, `desc`, `image`) VALUES ('$id','$id_tenant', '$name', '$desc', '$dirUpload.$namaFile')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
     header("Location: input.php");
