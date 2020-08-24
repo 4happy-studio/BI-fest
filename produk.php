82ba00c1f3c795d8cb40928524c581a071c0a146
4happy-studio <4happy.studio@gmail.com>
2020-08-24 19:15:02 +0700

    Update produk.php

--- produk.php
+++ produk.php
@@ -162,6 +162,10 @@ www.4happy-studio.com -->
           if($result_tenant = mysqli_query($link, $sql_tenant)){
             if(mysqli_num_rows($result_tenant) > 0){
               while($row_tenant = mysqli_fetch_array($result_tenant)){
+                  echo '<video controls autoplay>
+                  <source src="assets\vid\Video Tenant Amarilia.mp4" type="video/mp4">
+                  Your browser does not support the video tag.
+                </video>';
                   echo '<iframe width="560" height="315" src="'.$row_tenant['vid'].'?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
                   echo '<h2><b>'.$row_tenant['name'].'</b></h2>';
                   echo '<p class="mb-0">'.$row_tenant['desc'].'</p>';
