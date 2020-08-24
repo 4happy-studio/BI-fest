bf81f963d7e57ec1cc26aede8897b8c9e26d3989
4happy-studio <4happy.studio@gmail.com>
2020-08-24 12:46:48 +0700

    tambah
    tambah video

--- produk.php
+++ produk.php
@@ -170,6 +170,9 @@ www.4happy-studio.com -->
           if($result_tenant = mysqli_query($link, $sql_tenant)){
             if(mysqli_num_rows($result_tenant) > 0){
               while($row_tenant = mysqli_fetch_array($result_tenant)){
+                  echo '<video width="320" height="240" controls>
+                  <source src="'.$row['vid'].'" type="video/mp4">
+                </video>';
                   echo '<h2><b>'.$row_tenant['name'].'</b></h2>';
                   echo '<p class="mb-0">'.$row_tenant['desc'].'</p>';
               }
