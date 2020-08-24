3a821c837ffb403dbcd05aa242f5dc4ed6a8e22f
4happy-studio <4happy.studio@gmail.com>
2020-08-24 12:37:56 +0700

    input video
    vid

--- input_tenant.php
+++ input_tenant.php
@@ -1,8 +1,31 @@
 <?php
+
+
+// ambil data file
+$namaFile = $_FILES['vid']['name'];
+$namaSementara = $_FILES['vid']['tmp_name'];
+
+// tentukan lokasi file akan dipindahkan
+$dirUpload = "assets/vid/tenant/";
+
+// pindahkan file
+$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
+
+if ($terupload) {
+    echo "Upload berhasil!<br/>";
+    echo "Link: <a href='".$dirUpload.$namaFile."'>".$namaFile."</a>";
+} else {
+    echo "Upload Gagal!";
+}
+
+?>
+
+<?php
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("bikepriexpo.com:3306", "bikepri_user", "exploit9827", "bikepri_produk");
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -13,10 +36,10 @@ $id = mysqli_real_escape_string($link, $_REQUEST['id']);
 $name = mysqli_real_escape_string($link, $_REQUEST['name']);
 $desc = mysqli_real_escape_string($link, $_REQUEST['desc']);
 $cluster = mysqli_real_escape_string($link, $_REQUEST['cluster']);
-$vid = mysqli_real_escape_string($link, $_REQUEST['vid']);
+
 
 // Attempt insert query execution
-$sql = "INSERT INTO tenant (`id`, `name`, `desc`, `cluster`, `vid`) VALUES ('$id', '$name', '$desc', '$cluster', '$vid')";
+$sql = "INSERT INTO tenant (`id`, `name`, `desc`, `cluster`, `vid`) VALUES ('$id', '$name', '$desc', '$cluster', '$dirUpload$namaFile')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
     header("Location: input.php");
