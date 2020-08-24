494fb4b5fc910ea8143554a1cdfe4c0d58b36303
4happy-studio <4happy.studio@gmail.com>
2020-08-24 18:16:04 +0700

    update major
    major

--- produk.php
+++ produk.php
@@ -162,11 +162,7 @@ www.4happy-studio.com -->
           if($result_tenant = mysqli_query($link, $sql_tenant)){
             if(mysqli_num_rows($result_tenant) > 0){
               while($row_tenant = mysqli_fetch_array($result_tenant)){
-                  echo '<iframe src="'.$row_tenant['vid'].'" frameborder="0" loop="loop" allow="accelerometer" class="embed-responsive-item video-fluid" allow="autoplay; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
-                  <video id="ocScreencapVideo" autoplay="autoplay" loop="loop" playsinline="playsinline" preload="metadata" data-aos="fade-up">
-                  <source src="'.$row_tenant['vid'].'" type="video/mp4">
-                    Your browser does not support MP4 Format videos or HTML5 Video.
-                  </video>';
+                  echo '<iframe src="'.$row_tenant['vid'].'?autoplay=1" frameborder="0" loop="loop" allow="accelerometer" class="embed-responsive-item video-fluid" allow="autoplay; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>';
                   echo '<h2><b>'.$row_tenant['name'].'</b></h2>';
                   echo '<p class="mb-0">'.$row_tenant['desc'].'</p>';
               }
