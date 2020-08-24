7c1db7def43f890196f3a80878c735aa7391b9d2
4happy-studio <4happy.studio@gmail.com>
2020-08-24 09:38:07 +0700

    edit urlproduk
    tes

--- produk.php
+++ produk.php
@@ -209,9 +209,7 @@ www.4happy-studio.com -->
             echo '<p class="card-text">'. $row['desc'].'</p>';
             //echo '<img class="img-fluid" src="'. $row['image'].'">';
             echo '</div>';
-            echo '<div class="card-footer">
-            <a href="'.$row['urlproduk'].'" class="btn btn-lg btn-block btn-outline-warning btn-square align-items-center">Beli Disini</a>
-          </div>';
+            echo '<a href="'.$row['urlproduk'].'">Beli Disini</a>';
           echo '</div>
           </div>';
         }
