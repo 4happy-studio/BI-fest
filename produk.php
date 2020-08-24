5c7b17fd4859d6402ace73fdbbd5e28a4155dd91
4happy-studio <4happy.studio@gmail.com>
2020-08-24 14:49:30 +0700

    autoplay
    play

--- produk.php
+++ produk.php
@@ -171,7 +171,10 @@ www.4happy-studio.com -->
             if(mysqli_num_rows($result_tenant) > 0){
               while($row_tenant = mysqli_fetch_array($result_tenant)){
                   echo '<div class="embed-responsive embed-responsive-16by9"> 
-                  <iframe src="'.$row_tenant['vid'].'" frameborder="0" allow="accelerometer" class="embed-responsive-item video-fluid" autoplay encrypted-media; gyroscope picture-in-picture allowfullscreen></iframe> 
+                  <iframe src="'.$row_tenant['vid'].'" frameborder="0" allow="accelerometer" class="embed-responsive-item video-fluid" autoplay encrypted-media; gyroscope picture-in-picture allowfullscreen></iframe>
+                  <video playsinline="playsinline" autoplay poster="assets/img/syahfest.jpg" loop="loop" >
+                  <source src="'.$row_tenant['vid'].'" type="video/mp4">    
+                  </video> 
                   </div>';
                   echo '<h2><b>'.$row_tenant['name'].'</b></h2>';
                   echo '<p class="mb-0">'.$row_tenant['desc'].'</p>';
