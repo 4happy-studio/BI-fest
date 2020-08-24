486f59d8004c955fef61a3c8abf27ea4f9615ea3
4happy-studio <4happy.studio@gmail.com>
2020-08-24 15:47:08 +0700

    tes
    tesss

--- produk.php
+++ produk.php
@@ -176,12 +176,10 @@ var autoPlayVideo = document.getElementById("ocScreencapVideo");
           if($result_tenant = mysqli_query($link, $sql_tenant)){
             if(mysqli_num_rows($result_tenant) > 0){
               while($row_tenant = mysqli_fetch_array($result_tenant)){
-                  echo '<div class="embed-responsive embed-responsive-16by9"> 
-                  <iframe id="ocScreencapVideo" autoplay="autoplay" muted="muted" loop="loop" playsinline="playsinline" preload="metadata" data-aos="fade-up">
+                  echo '<video id="ocScreencapVideo" autoplay="autoplay" muted="muted" loop="loop" playsinline="playsinline" preload="metadata" data-aos="fade-up">
                   <source src="'.$row_tenant['vid'].'" type="video/mp4">
                     Your browser does not support MP4 Format videos or HTML5 Video.
-                  </iframe>
-                  </div>';
+                  </video>';
                   echo '<h2><b>'.$row_tenant['name'].'</b></h2>';
                   echo '<p class="mb-0">'.$row_tenant['desc'].'</p>';
               }
