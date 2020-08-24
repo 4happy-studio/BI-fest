3e3a51b2a22c3b0ca181a04ef08b898615aef7ff
4happy-studio <4happy.studio@gmail.com>
2020-08-24 19:39:55 +0700

    Update produk.php

--- produk.php
+++ produk.php
@@ -170,10 +170,10 @@ www.4happy-studio.com -->
               echo '<script>
           
               // 2. This code loads the IFrame Player API code asynchronously.
-     var tag = document.createElement('script');
+     var tag = document.createElement("script");
    
    tag.src = "https://www.youtube.com/iframe_api";
-   var firstScriptTag = document.getElementsByTagName('script')[0];
+   var firstScriptTag = document.getElementsByTagName("script")[0];
    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
    
    // 3. This function creates an <iframe> (and YouTube player)
