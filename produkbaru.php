df8281a81b09a1169e0a2399b132576fb2a89c4a
4happy-studio <4happy.studio@gmail.com>
2020-08-27 10:16:00 +0700

    update
    update

--- produkbaru.php
+++ produkbaru.php
@@ -197,27 +197,30 @@ www.4happy-studio.com -->
           
             echo '
             
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="'.$row['image'].'" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="'.$row['image'].'" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="'.$row['image'].'" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>';
+           
+                  
+            <div class="item 3d col-sm-6 col-md-4 col-lg-4 mb-4">
+            <div class="card">
+            
+              <a href="#" class="item-wrap fancybox" onclick="img_box('.$row['image'].')">
+                <div class="work-info">
+                  <h3></h3>
+                  <p>Klik untuk fullscreen foto</p>
+                </div>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="'.$row['image'].'" alt="First slide">
+                  </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>';
             echo '</a>
             <div class="card-body text-center">';
             echo '<h5 class="card-title"><b>'. $row['name'].'</b></h5>';
